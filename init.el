78ecbcda0508efee4a648864ecc7256d829d7aa4
Patrick <synapse.and.syntax@gmail.com>
2011-12-01 13:35:28 -0500

    fixed haskell-mode oddities like C-c C-l and compiler choice

--- init.el
+++ init.el
@@ -7,6 +7,9 @@
 (tool-bar-mode -1) 
 (setq x-select-enable-clipboard t); why was this ever, ever disabled?
 (ido-mode t)
+(require 'inf-haskell)
+(custom-set-variables
+ '(haskell-program-name "ghci"))
 (defun pdf-with-okular ()
 (add-to-list 'TeX-output-view-style
 (quote ("^pdf$" "." "okular %o %(outpage)"))))
