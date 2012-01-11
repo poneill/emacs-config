a9a6fba820f2a275f2c12af37bb80986d478fde0
Patrick O'Neill <synapse.and.syntax@gmail.com>
2012-01-10 21:24:26 -0500

    corrected haskell background color in literate haskell mmm mode

--- init.el
+++ init.el
@@ -9,6 +9,12 @@
 (ido-mode t)
 (require 'inf-haskell)
 (custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
  '(haskell-program-name "ghci"))
 
 (setq Tex-PDF-mode t)
@@ -183,10 +189,14 @@
 	     'org-latex-right-paren)))
 
 ;Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
-(custom-set-variables
- '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
- '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
 
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
 
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(mmm-default-submode-face ((t (:background "gray25")))))
